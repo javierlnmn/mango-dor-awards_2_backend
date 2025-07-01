cab6105ab7651146783721b03a633072645f85c3
javierlnmn <jlm03123@gmail.com>
2025-07-01 16:43:21 +0200

    ✨ feat(voting): create admin display

--- voting/admin.py
+++ voting/admin.py
@@ -1,1 +1,25 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Vote
+
+
+class VoteInline(admin.TabularInline):
+    model = Vote
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "code")
+    search_fields = ("name", "code")
+    list_filter = ("name", "code")
+    exclude = ("winner",)
+
+    inlines = [VoteInline]
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("user", "candidate", "category", "points")
+    search_fields = ("user", "candidate", "category", "points")
+    list_filter = ("user", "candidate", "category", "points")
