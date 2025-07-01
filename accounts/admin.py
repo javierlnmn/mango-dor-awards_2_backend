6aed18c80093d8d3f8fefae107a19b43ab632652
javierlnmn <jlm03123@gmail.com>
2025-07-01 15:50:46 +0200

    ✨ feat(auth): add admin model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name")
+    search_fields = ("username", "first_name", "last_name")
+    list_filter = ("is_staff", "is_superuser", "is_active")
