48ae0c6ea9ed633630019dab53bf7bd956c16750
javierlnmn <jlm03123@gmail.com>
2025-07-01 17:02:06 +0200

    ✨ feat(candidates): create admin display

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,1 +1,42 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Candidate, CandidateImage, Gender, Nationality
+
+
+class CandidateImageInline(admin.TabularInline):
+    model = CandidateImage
+    extra = 1
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age", "gender", "nationalities")
+    search_fields = ("first_name", "last_name", "age", "gender", "nationalities")
+    list_filter = (
+        "first_name",
+        "last_name",
+        "age",
+        "gender",
+    )
+    prepopulated_fields = {"slug": ("first_name", "last_name")}
+
+    inlines = [CandidateImageInline]
+
+    def get_nationalities(self, obj):
+        return ", ".join(obj.nationalities)
+
+    get_nationalities.short_description = "Nationalities"
+
+
+@admin.register(Gender)
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ("code", "name")
+    search_fields = ("code", "name")
+    list_filter = ("code", "name")
+
+
+@admin.register(Nationality)
+class NationalityAdmin(admin.ModelAdmin):
+    list_display = ("code", "name")
+    search_fields = ("code", "name")
+    list_filter = ("code", "name")
